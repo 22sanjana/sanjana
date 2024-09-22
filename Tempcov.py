5c3a0a02e53f344b45694983504283e6ed62d92d
sanjana M R <sanjanamr324@gmail.com>
2024-09-22 22:39:59 +0530

    Add files via upload

---
+++ Tempcov.py
@@ -0,0 +1,5 @@
+Temp_Celcius=float(input("Enter the temperature in degree celcius "))
+Fahrenheit=(9/5)*Temp_Celcius+32
+Kelvin=Temp_Celcius+273.15
+print(f"Temperature is {Fahrenheit} F")
+print(f"Temperature is {Kelvin} K")
